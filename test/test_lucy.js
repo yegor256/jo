a990b1207e6a9ea664f1ceb58959f2cf78e45470
Yegor Bugayenko <yegor256@gmail.com>
2019-10-26 11:30:43 +0800

    #10 assert

--- test/test_lucy.js
+++ test/test_lucy.js
@@ -1,12 +1,15 @@
+var a = require('assert');
 var lucy = require('../src/lucy');
 
-var fake = {
-  printed: "",
-  log: function (text) {
-    this.printed = text;
-  }
-};
-lucy.hello("Louise", "fr", fake);
-if ("Bon jour, Louise" !== fake.printed) {
-  throw "lucy.hello() printed this: " + fake.printed;
-}
+describe('lucy', function() {
+  it('mocks the console for logging', function() {
+    var fake = {
+      printed: "",
+      log: function (text) {
+        this.printed = text;
+      }
+    };
+    lucy.hello("Louise", "fr", fake);
+    a.strictEqual("Bon jour, Louise", fake.printed);
+  });
+});
